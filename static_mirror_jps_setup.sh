1c10c3f4f3d333cfacaf9ffa22f2413d4fd422b4
Chris Blankenship <chrisb@reclaimhosting.com>
2023-05-04 16:57:05 -0400

    will sudo work?

--- static_mirror_jps_setup.sh
+++ static_mirror_jps_setup.sh
@@ -7,14 +7,17 @@ EMAIL_TO_ALERT=$2
 mkdir -p /var/lib/nginx/mirror_temp
 
 # Perform inital mirroring of site
-screen -dm bash -c "httrack --mirror 'https://$URL_TO_MIRROR' -\"*?*\" -O /var/lib/nginx/mirror_temp && rsync /usr/bin/rsync -azP -q  /var/lib/nginx/mirror_temp/$URL_TO_MIRROR /var/www/webroot/ROOT/"
+if [[ -d /var/lib/nginx/mirror_temp ]]; then
+	httrack --mirror 'https://$URL_TO_MIRROR' -"*?*" -O /var/lib/nginx/mirror_temp && \
+	rsync -azP -q  /var/lib/nginx/mirror_temp/$URL_TO_MIRROR/ /var/www/webroot/ROOT/
+fi
 
-# Copy static mirror script
-curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mirror.sh > /var/lib/nginx/static_mirror.sh
-
-# Set up cronjob for repeated syncs
-crontab -l > /var/lib/nginx/new_cron
-echo "@daily /usr/bin/bash /var/lib/nginx/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /var/lib/nginx/new_cron
-echo "" >> /var/lib/nginx/new_cron
-crontab  /var/lib/nginx/new_cron
-rm /var/lib/nginx/new_cron
+# Copy static mirror script and set up cronjob
+if [[ -d /var/lib/nginx/mirror_temp/$URL_TO_MIRROR ]]; then
+	curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mirror.sh > /var/lib/nginx/static_mirror.sh && \
+	crontab -l > /var/lib/nginx/new_cron && \
+	echo "@daily /usr/bin/bash /var/lib/nginx/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /var/lib/nginx/new_cron && \
+	echo "" >> /var/lib/nginx/new_cron && \
+	crontab  /var/lib/nginx/new_cron && \
+	rm /var/lib/nginx/new_cron && \
+fi 
