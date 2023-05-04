e8591c75f2a8e52bb57a0322ef2207d7c51b9856
Chris Blankenship <chrisb@reclaimhosting.com>
2023-05-04 16:23:47 -0400

    almost, forgot to create that dir tho

--- static_mirror_jps_setup.sh
+++ static_mirror_jps_setup.sh
@@ -3,6 +3,9 @@
 URL_TO_MIRROR=$1
 EMAIL_TO_ALERT=$2
 
+# Create mirror_temp dir
+mkdir -p /var/lib/nginx/mirror_temp
+
 # Perform inital mirroring of site
 screen -dm bash -c "httrack --mirror 'https://$URL_TO_MIRROR' -\"*?*\" -O /var/lib/nginx/mirror_temp && rsync /usr/bin/rsync -azP -q  /var/lib/nginx/mirror_temp/$URL_TO_MIRROR /var/www/webroot/ROOT/"
 
@@ -12,5 +15,6 @@ curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mi
 # Set up cronjob for repeated syncs
 crontab -l > /var/lib/nginx/new_cron
 echo "@daily /usr/bin/bash /var/lib/nginx/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /var/lib/nginx/new_cron
+echo "" >> /var/lib/nginx/new_cron
 crontab  /var/lib/nginx/new_cron
 rm /var/lib/nginx/new_cron
