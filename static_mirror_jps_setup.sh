1c70d710abfed3708a2e08089c50156ce4c5bcd2
Chris Blankenship <chrisb@reclaimhosting.com>
2023-05-04 16:06:42 -0400

    also gotta do some stuff with runuser

--- static_mirror_jps_setup.sh
+++ static_mirror_jps_setup.sh
@@ -7,13 +7,13 @@ EMAIL_TO_ALERT=$2
 yum install httrack screen -y
 
 # Perform inital mirroring of site
-screen -dm bash -c "httrack --mirror 'https://$URL_TO_MIRROR' -\"*?*\" -O /var/lib/nginx/mirror_temp && rsync /usr/bin/rsync -azP -q  /var/lib/nginx/mirror_temp/$URL_TO_MIRROR /var/www/webroot/ROOT/"
+screen -dm bash -c "httrack --mirror 'https://$URL_TO_MIRROR' -\"*?*\" -O /var/lib/nginx/mirror_temp && chown -R nginx:nginx && rsync /usr/bin/rsync -azP -q  /var/lib/nginx/mirror_temp/$URL_TO_MIRROR /var/www/webroot/ROOT/"
 
 # Copy static mirror script
 curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mirror.sh > /var/lib/nginx/static_mirror.sh
 
 # Set up cronjob for repeated syncs
-crontab -l > mycron
-echo "@daily /usr/bin/bash /var/lib/nginx/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> mycron
-crontab mycron
-rm mycron
+runuser -l jelastic -c "crontab -l > /var/lib/nginx/new_cron"
+echo "@daily /usr/bin/bash /var/lib/nginx/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /var/lib/nginx/new_cron
+runuser -l jelastic -c "crontab  /var/lib/nginx/new_cron"
+rm /var/lib/nginx/new_cron
