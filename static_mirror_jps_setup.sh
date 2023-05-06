5e7f307052543f751002322e74d9fe79819d1006
Chris Blankenship <chrisb@reclaimhosting.com>
2023-05-05 21:15:49 -0400

    removed some checks

--- static_mirror_jps_setup.sh
+++ static_mirror_jps_setup.sh
@@ -7,20 +7,16 @@ EMAIL_TO_ALERT=$2
 mkdir -p /root/mirror_temp
 
 # Perform inital mirroring of site
-if [[ -d /root/mirror_temp ]]; then
-	httrack --mirror "https://$URL_TO_MIRROR" -"*?*" -O /root/mirror_temp && \
-	rsync -azPI -q  /root/mirror_temp/$URL_TO_MIRROR/ /usr/share/nginx/html/
-fi
+httrack --mirror "https://$URL_TO_MIRROR" -"*?*" -O /root/mirror_temp
+rsync -azPI -q  /root/mirror_temp/$URL_TO_MIRROR/ /usr/share/nginx/html/
 
 # Copy static mirror script and set up cronjob
-if [[ -d /root/mirror_temp/$URL_TO_MIRROR ]]; then
-	curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mirror.sh > /root/static_mirror.sh && \
-	crontab -l > /root/new_cron && \
-	echo "@daily /usr/bin/bash /root/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /root/new_cron && \
-	echo "" >> /root/new_cron && \
-	crontab  /root/new_cron && \
-	rm /root/new_cron && \
-fi 
+curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mirror.sh > /root/static_mirror.sh
+crontab -l > /root/new_cron
+echo "@daily /usr/bin/bash /root/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /root/new_cron
+echo "" >> /root/new_cron
+crontab  /root/new_cron
+rm /root/new_cron
 
 # Restart Nginx
 systemctl restart nginx
