96b1bf10e946e5aa66e47d3314d7492f258bb80c
Chris Blankenship <chrisb@reclaimhosting.com>
2023-05-06 21:21:06 -0400

    dynamic paths for portability and whatnot

--- static_mirror.sh
+++ static_mirror.sh
@@ -1,42 +1,48 @@
 #!/usr/bin/env bash
 
-TIMESTAMP=$(date +%s)
+# User/Server/Script-specific variables
+TIMESTAMP=$(/bin/date +%s)
 WORKING_DIR="/root/mirror_temp"
 TARGET_DIR="/usr/share/nginx/html"
 TARGET_SITE=$1
-THIS_HOST=$(hostname)
-THIS_USER=$(whoami)
+THIS_HOST=$(/bin/hostname)
+THIS_USER=$(/usr/bin/whoami)
 ALERT_EMAIL=$2
 
-# Initial Mirror
-# httrack --mirror "https://$TARGET_SITE" -%q0 -O /root/mirror_temp
+# Paths to binaries for portability
+CURL_PATH=$(/usr/bin/which curl)
+HTTRACK_PATH=$(/usr/bin/which httrack)
+TOUCH_PATH=$(/usr/bin/which touch)
+RSYNC_PATH=$(/usr/bin/which rsync)
+ECHO_PATH=$(/usr/bin/which echo)
+CAT_PATH=$(/usr/bin/which cat)
+SENDMAIL_PATH=$(/usr/bin/which sendmail)
+RM_PATH=$(/usr/bin/which rm)
 
 # Get status code of target site
-STATUS_CODE=$(/usr/bin/curl -o /dev/null -s -w "%{http_code}\n" https://$TARGET_SITE)
-STATIC_FLAG=$(/usr/bin/curl -o /dev/null -s -w "%{http_code}\n" https://$TARGET_SITE/static_flag.html)
+STATUS_CODE=$($CURL_PATH -o /dev/null -s -w "%{http_code}\n" https://$TARGET_SITE)
+STATIC_FLAG=$($CURL_PATH -o /dev/null -s -w "%{http_code}\n" https://$TARGET_SITE/static_flag.html)
 
 # Check if main site is returning status code "200" and the static flag is a "404"
 if [[ $STATUS_CODE == "200" && $STATIC_FLAG == "404" ]]; then
 	# And that the various dirs exist
 	if [[ -d $WORKING_DIR && -d $TARGET_DIR ]]; then
 		# Then update the mirror of the site with `httrack`	
-		/usr/bin/cd $WORKING_DIR && \
-		/usr/bin/httrack --update "https://$TARGET_SITE" -%q0 -O $WORKING_DIR && \
-		/usr/bin/cd -
+		$HTTRACK_PATH --update "https://$TARGET_SITE" -%q0 -O $WORKING_DIR
 		# Touch the static flag
-		/usr/bin/touch $WORKING_DIR/$TARGET_SITE/static_flag.html
+		$TOUCH_PATH $WORKING_DIR/$TARGET_SITE/static_flag.html
 		# And move the files to the target directory
-		/usr/bin/rsync -azPI --delete-during -q $WORKING_DIR/$TARGET_SITE/ $TARGET_DIR/
+		$RSYNC_PATH -azPI --delete-during -q $WORKING_DIR/$TARGET_SITE/ $TARGET_DIR/
 	fi
 else
-	/usr/bin/touch $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "Subject: Static Mirror Failed" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "From: $THIS_HOST <$THIS_USER@$THIS_HOST>" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "To: $ALERT_EMAIL <$ALERT_EMAIL>" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "Static Mirroring For $TARGET_SITE Failed" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "Status Code: $STATUS_CODE" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/echo "Static Flag: $STATIC_FLAG" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
-	/usr/bin/cat $WORKING_DIR/EMAIL_$TIMESTAMP.log | /usr/sbin/sendmail $ALERT_EMAIL
-	/usr/bin/rm -f $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$TOUCH_PATH $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "Subject: Static Mirror Failed" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "From: $THIS_HOST <$THIS_USER@$THIS_HOST>" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "To: $ALERT_EMAIL <$ALERT_EMAIL>" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "Static Mirroring For $TARGET_SITE Failed" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "Status Code: $STATUS_CODE" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$ECHO_PATH "Static Flag: $STATIC_FLAG" >> $WORKING_DIR/EMAIL_$TIMESTAMP.log
+	$CAT_PATH $WORKING_DIR/EMAIL_$TIMESTAMP.log | $SENDMAIL_PATH $ALERT_EMAIL
+	$RM_PATH -f $WORKING_DIR/EMAIL_$TIMESTAMP.log
 fi
--- static_mirror_jps_setup.sh
+++ static_mirror_jps_setup.sh
@@ -13,7 +13,7 @@ rsync -azPI -q  /root/mirror_temp/$URL_TO_MIRROR/ /usr/share/nginx/html/
 # Copy static mirror script and set up cronjob
 curl https://raw.githubusercontent.com/cblanke2/static-mirror-jps/main/static_mirror.sh > /root/static_mirror.sh
 crontab -l > /root/new_cron
-echo "@daily /usr/bin/bash /root/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /root/new_cron
+echo "@daily /bin/bash /root/static_mirror.sh '$URL_TO_MIRROR' '$EMAIL_TO_ALERT'" >> /root/new_cron
 echo "" >> /root/new_cron
 crontab  /root/new_cron
 rm /root/new_cron
@@ -27,7 +27,7 @@ yes "  " | openssl req -newkey rsa:4096 -x509 -sha256 -days 7300 -nodes -out /ro
 touch /etc/nginx/conf.d/ssl.conf
 echo "server {" >> /etc/nginx/conf.d/ssl.conf
 echo "    listen              443 ssl;" >> /etc/nginx/conf.d/ssl.conf
-echo "    server_name         $URL_TO_MIRROR;" >> /etc/nginx/conf.d/ssl.conf
+echo "    server_name         localhost;" >> /etc/nginx/conf.d/ssl.conf
 echo "    ssl_certificate     /root/ssl/selfsigned.crt;" >> /etc/nginx/conf.d/ssl.conf
 echo "    ssl_certificate_key /root/ssl/selfsigned.key;" >> /etc/nginx/conf.d/ssl.conf
 echo "    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;" >> /etc/nginx/conf.d/ssl.conf
